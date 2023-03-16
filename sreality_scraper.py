554d75497d13e18da9b993255bec5a035a8865ef
Josef Kulhanek <kulhanek.josef@gmail.com>
2023-03-16 15:16:45 +0800

    Update sreality_scraper.py

--- sreality_scraper.py
+++ sreality_scraper.py
@@ -1,56 +1,80 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-from sqlalchemy import create_engine
 import sqlite3
 from datetime import datetime
 
+# Constants
 BASE_URL = "https://www.sreality.cz"
-SEARCH_URL = "/hledani/prodej/byty/praha?strana="
+SEARCH_URL = "/hledani/prodej/byty"
 
-def get_property_links(page_number):
-    url = f"{BASE_URL}{SEARCH_URL}{page_number}"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    property_links = [a['href'] for a in soup.select('.property a')]
-    return property_links
+# Connect to the SQLite database file
+conn = sqlite3.connect('/root/sreality-db/sreality_db.sqlite3')
+cursor = conn.cursor()
 
-def get_property_details(link):
-    response = requests.get(f"{BASE_URL}{link}")
-    soup = BeautifulSoup(response.text, "html.parser")
-    
-    price = soup.select_one('.norm-price').text.strip()
-    title = soup.select_one('h1').text.strip()
-    address = soup.select_one('.locality').text.strip()
-    
-    details = soup.select_one('.params-list').find_all('li')
-    detail_dict = {}
-    for detail in details:
-        key, value = [item.strip() for item in detail.text.split(':', 1)]
-        detail_dict[key] = value
+# Create the properties table if it doesn't exist
+cursor.execute('''CREATE TABLE IF NOT EXISTS properties (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    link TEXT,
+    price INTEGER,
+    price_per_sqm INTEGER,
+    location TEXT,
+    size TEXT,
+    rooms INTEGER,
+    floor INTEGER,
+    building_type TEXT,
+    ownership TEXT,
+    property_age TEXT,
+    property_condition TEXT,
+    listing_date TEXT,
+    agency_name TEXT,
+    timestamp DATETIME
+)''')
 
-    detail_dict['title'] = title
-    detail_dict['price'] = price
-    detail_dict['address'] = address
-    detail_dict['link'] = f"{BASE_URL}{link}"
-    detail_dict['scraped_at'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    
-    return detail_dict
+# Scrape the properties
+response = requests.get(BASE_URL + SEARCH_URL)
+soup = BeautifulSoup(response.text, 'html.parser')
+properties = soup.select('.property')
 
-def main():
-    # Scrape property listings
-    all_properties = []
-    for i in range(1, 3):  # Change the range as needed (e.g., `range(1, 101)` for 100 pages)
-        property_links = get_property_links(i)
-        for link in property_links:
-            property_details = get_property_details(link)
-            all_properties.append(property_details)
-            print(f"Scraped: {property_details['title']}")
+for property in properties:
+    title = property.select_one('.property__title').text.strip()
+    link = BASE_URL + property.select_one('.property__title a')['href']
+    price = int(property.select_one('.property__price').text.strip().replace('Kč', '').replace(' ', ''))
+    location = property.select_one('.property__location').text.strip()
+    size = property.select_one('.property__info').text.strip()
+    price_per_sqm = int(price / float(size.split(' ')[0]))
 
-    # Save data to a SQLite database
-    df = pd.DataFrame(all_properties)
-    engine = create_engine('sqlite:///data/property_listings.db')
-    df.to_sql('property_listings', engine, if_exists='append', index=False)
+    # Additional parameters
+    rooms = int(property.select_one('.property__rooms').text.strip().split(' ')[0])
+    floor_info = property.select_one('.property__floor').text.strip().split('/')
+    floor = int(floor_info[0].strip().split(' ')[0])
+    building_type = floor_info[1].strip()
+    ownership = property.select_one('.property__ownership').text.strip()
 
-if __name__ == "__main__":
-    main()
+    # Scrape additional details from the property page
+    property_response = requests.get(link)
+    property_soup = BeautifulSoup(property_response.text, 'html.parser')
+
+    property_age = property_soup.select_one('.property__info .property__age')
+    property_age = property_age.text.strip() if property_age else None
+
+    property_condition = property_soup.select_one('.property__info .property__condition')
+    property_condition = property_condition.text.strip() if property_condition else None
+
+    listing_date = property_soup.select_one('.property__listing-date time')
+    listing_date = listing_date['datetime'] if listing_date else None
+
+    agency_name = property_soup.select_one('.property__seller .seller__name')
+    agency_name = agency_name.text.strip() if agency_name else None
+
+    timestamp = datetime.now()
+
+    # Insert property details into the database
+    cursor.execute('''
+        INSERT INTO properties (title, link, price, price_per_sqm, location, size, rooms, floor, building_type, ownership, property_age, property_condition, listing_date, agency_name, timestamp)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (title, link, price, price_per_sqm, location, size, rooms, floor, building_type, ownership, property_age, property_condition, listing_date, agency_name, timestamp))
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
