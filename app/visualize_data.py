7792b3a14c35d61cf9699b4e1aa41b3f95d8341d
Josef Kulhanek <kulhanek.josef@gmail.com>
2023-03-17 15:15:35 +0800

    Update visualize_data.py

--- app/visualize_data.py
+++ app/visualize_data.py
@@ -14,5 +14,19 @@ def visualize_data():
     # Display the data in a tabular format
     print(df)
 
+def export_data_to_csv():
+    # Connect to the SQLite database
+    conn = sqlite3.connect('/root/sreality-db/app/sreality_db.sqlite3')
+
+    # Read data from the database into a pandas DataFrame
+    df = pd.read_sql_query("SELECT * FROM property", conn)
+
+    # Close the database connection
+    conn.close()
+
+    # Export the data to a CSV file
+    df.to_csv('property_data.csv', index=False)
+
 if __name__ == "__main__":
     visualize_data()
+    export_data_to_csv()
